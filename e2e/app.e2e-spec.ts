3acedb4dc733c963f9b19b9932ff34691fdeafbb
Unknown <vivimendoza16@gmail.com>
2018-05-20 02:08:32 -0500

    TDD Tests Funcionales Pasados

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -10,44 +10,105 @@ describe('conectate App', () => {
 
   it('should display Conectate Page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Conectate');
+    expect(page.navigateBackToHome()).toEqual('Buscar');
   });
 
   it('should Make Valid Search', () => {
-    var EC=ProtractorExpectedConditions;
     page.navigateTo();
     page.enterValidKeyWords('Moodle');
-    expect(page.getSearch()).toEqual('Conectate');
+    expect(page.getSearch()).toEqual('Moodle');
+  });
+
+  it('should navigate to tool detail', () => {
+    browser.sleep(3000);
+    expect(page.navigateToDetail(2)).toEqual('Angular');
   });
 
   it('should navigate to add tool page', () => {
     page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
     expect(page.navigateToAddToolPage()).toEqual('Agregar herramienta');
   });
 
+  it('should edit and cancel tool draft', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
+    page.navigateToAddToolPage();
+    page.editToolDraftInputs();
+    page.selectToolDraftOptions();
+    page.cancelToolDraft();
+    expect(page.closeGitSesion()).toEqual('¿Eres miembro conectate?');
+  });
+
+  it('should edit and save tool draft', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
+    page.navigateToAddToolPage();
+    page.editToolDraftInputs();
+    page.selectToolDraftOptions();
+    page.selectToolDraftSO();
+    expect(page.saveToolDraft()).toEqual('Jira Confluence');
+  });
+
+  it('should filter tools', () => {
+    page.navigateTo();
+    expect(page.chooseFilterOptions()).toEqual('Cacoo');
+  });
+
+  it('should verified tool draft', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
+    page.verifyDraft();
+    expect(page.closeGitSesion()).toEqual('¿Eres miembro conectate?');
+  });
+
+  it('should publish tool draft', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToAdminProfile();
+    page.publishDraft();
+    page.closeAdminSesion();
+    expect(page.getNewTool()).toEqual('Jira Confluence');
+  });
+
+  /*it('should add example', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
+    page.navigateToAddExamplePage();
+    page.editExampleDraftInputs();
+    page.selectExampleDraftOptions();
+    page.editExampleDraftResource();
+    expect(page.saveExampleDraft()).toEqual('Jira Confluence');
+  });*/
+
+  it('should add tutorial', () => {
+    page.navigateTo();
+    page.showMenu();
+    page.navigateToGitProfile();
+    page.navigateToAddTutorialPage();
+    page.editTutorialDraftInputs();
+    expect(page.saveTutorialDraft()).toEqual('Tutorial Angular');
+  });
+  
+
+
+
+
+
+
+
+
+  /*
   it('should navigate to add example page', () => {
     page.navigateTo();
     page.navigateToDetail(2);
     browser.sleep(2000);
     expect(page.navigateToAddExamplePage()).toEqual('Moodle - foros');
-  });
-
-  it('should navigate to tool detail', () => {
-    browser.sleep(3000);
-    page.navigateToDetail(2);
-    expect(page.navigateToDetail(2)).toEqual('angular');
-  });
-
-  
-  it('should edit and cancel tool draft', () => {
-    page.navigateTo();
-    //page.getMenu();
-    page.navigateToAddToolPage();
-    page.editToolDraftInputs();
-    page.selectToolDraftOptions();
-    page.cancelToolDraft();
-    browser.sleep(2000)
-    expect(element(by.cssContainingText('.home-title','Conectate')));
   });
 
   it('should edit example draft', () => {
@@ -111,28 +172,10 @@ describe('conectate App', () => {
     page.selectToolDraftOptions();
     page.saveToolDraft();
     expect(page.navigateToAddToolPage()).toEqual('Agregar herramienta');
-  });
+  });*/
 
-  it('should save tool draft and appear in drafts', () => {
-    page.navigateTo();
-    //page.getMenu();
-    page.navigateToAddToolPage();
-    page.editToolDraftInputs();
-    page.selectToolDraftOptions();
-    page.saveToolDraft();
-    page.returnHome();
-    page.navigateToDraftsView();
-    expect(page.navigateToDraftsView()).toBe('ToolName');
-  });
+
 
   
-  it('should publish tool draft in home ', () => {
-    page.navigateTo();
-    page.navigateToDrafts();
-    page.approveDraft();
-    page.publishDraft();
-    page.returnHome();
-    expect(page.returnHome()).toEqual('ToolName');
-  });
 
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -8,27 +8,20 @@ export class AppPage {
 
   navigateToDetail(id: number) {
     browser.get('/tool/' + id + '/detail');
-    return element(by.xpath('//h4[contains(text(), "angular")]')).getText();
+    return element(by.xpath('//h2[contains(text(), "Angular")]')).getText();
 
   }
 
   navigateToDetailName() {
-    element(by.linkText('angular')).click();
-    return element(by.xpath('//h4[contains(text(), "angular")]')).getText();
+    element(by.xpath('//a[contains(text(), "Angular")]')).click();
+    return element(by.xpath('//h2[contains(text(), "Angular")]')).getText();
 
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
-  }
-
-  hideMenu() {
-    browser.sleep(2000)
-    return element(by.css('.mat-icon-button')).click()
-  }
-
-  getOptions() {
-    return element.all(by.css('app-root a')).getText();
+  navigateBackToHome() {
+    element(by.css('.navbar-brand img')).click();
+    //element(by.cssContainingText('a', 'Inicio')).click();
+    return element(by.id('search')).getText();
   }
 
   enterValidKeyWords(search: string) {
@@ -36,42 +29,53 @@ export class AppPage {
     browser.sleep(2000);
     element(by.buttonText('Buscar')).click();
     browser.sleep(2000);
-
   }
 
   getSearch() {
     //return element(by.tagName('input')).getText();
-    return element(by.css('app-root h1')).getText();
+    //return element(by.css('app-root h1')).getText();
+    return element(by.cssContainingText('a', 'Moodle')).getText();
   }
 
+  showMenu() {
+    browser.sleep(2000)
+    element(by.cssContainingText('a','¿Eres miembro conectate?')).click();
+    return element.all(by.css('.dropdown-item')).getText();
+  }
+
+  /*getOptions() {
+    return element.all(by.css('.dropdown-item')).getText();
+  }*/
+
+  navigateToGitProfile(){
+    element(by.cssContainingText('a','Inicio Asesor')).click();
+    return element(by.cssContainingText('a','Carlos')).getText();
+  }
+  
+  BeInGitProfile(){
+    return element(by.cssContainingText('a','Carlos')).getText();
+  }
+
+  navigateToAdminProfile(){
+    element(by.cssContainingText('a','Inicio Admin')).click();
+    return element(by.cssContainingText('a','Santiago')).getText();
+  }
+  
+  BeInAdminProfile(){
+    return element(by.cssContainingText('a','Santiago')).getText();
+  }
   navigateToAddToolPage() {
-    browser.get('/tool');
-    return element(by.css('app-root h2')).getText();
-  }
-
-  navigateToDrafts(){
-    browser.get('/tool/lista');
-    return element(by.css('.row h2')).getText();
-
-  }
-
-  navigateBackToHome() {
-    element(by.linkText('Inicio')).click();
-    return element(by.css('app-root h1')).getText();
-  }
-
-  navigateToAddExamplePage() {
-    element(by.id('Add')).click();
-    return element(by.css('.col-md-12 h1')).getText();
+    //browser.get('/tool');
+    element(by.cssContainingText('span','Agregar Herramienta')).click();
+    return element(by.css('app-root h4')).getText();
   }
 
   editToolDraftInputs() {
-    element(by.id('txtNombre')).sendKeys('ToolName');
-    element(by.id('txtKeyWords')).sendKeys('ToolName');
-    element(by.id('mat-input-2')).sendKeys('Desc');
-    element(by.id('mat-input-3')).sendKeys('Restrict');
-    element(by.id('txtLicense')).sendKeys('Lic');
-    element(by.id('txtVersion')).sendKeys('Ver');
+    element(by.id('txtNombre')).sendKeys('Jira Confluence');
+    element(by.id('txtKeyWords')).sendKeys('Administrador, archivos, Control');
+    element(by.id('mat-input-2')).sendKeys('Herramienta para control de avance y documentación');
+    element(by.id('mat-input-3')).sendKeys('Ninguna');
+    element(by.id('txtVersion')).sendKeys('2');
     element(by.id('txtUrl')).sendKeys('https://www.youtube.com/watch?v=DzXlZPsOiOk');
   }
 
@@ -79,19 +83,152 @@ export class AppPage {
     element(by.id('state')).click();
     browser.sleep(2000);
     element(by.xpath('//span[contains(text(), "Borrador")]')).click();
+    element(by.id('txtLicense')).click();
+    browser.sleep(2000);
+    element(by.xpath('//span[contains(text(), "Abierta")]')).click();
+    browser.sleep(2000);
+    element(by.id('txtIntegrationLms')).click();
+    browser.sleep(2000);
+    element(by.xpath('//span[contains(text(), "No")]')).click();
+    
+   }
+
+   selectToolDraftSO() {
     element(by.id('SO')).click();
     browser.sleep(2000);
     element(by.xpath('//span[contains(text(), "Windows")]')).click();
-    
     element(by.css('.cdk-overlay-backdrop.cdk-overlay-transparent-backdrop.cdk-overlay-backdrop-showing')).click();
+
    }
 
-  saveToolDraft(){
+
+   saveToolDraft(){
     browser.sleep(2000);
     element(by.id('btnGuardar')).click(); 
-    element(by.xpath('//span[contains(text(), "No")]')).click();
-    return element(by.css('app-root h2')).getText();
+    browser.sleep(2000);
+    element(by.id('NoEdit')).click();
+    browser.sleep(2000);
+    return element(by.cssContainingText('a', 'Jira Confluence')).getText();
   }
+
+  cancelToolDraft(){
+    element(by.id('Cancelar')).click();   
+    return element(by.id('search')).getText(); 
+  }
+
+  chooseFilterOptions(){
+    element(by.cssContainingText('span','No')).click();
+    browser.sleep(2000);
+    element(by.cssContainingText('span','Windows')).click();
+    element(by.cssContainingText('span','Abierta')).click();
+    element(by.cssContainingText('span','1 o +')).click();
+    return element(by.cssContainingText('a', 'Cacoo')).getText();
+  }
+
+  verifyDraft(){
+    //let draft=element.all(by.tagName('a')).last();
+    //draft.click();
+    element(by.cssContainingText('a', 'Jira Confluence')).click();
+    browser.sleep(2000);
+    element(by.id('btnVerificar')).click();
+  }
+
+  publishDraft(){
+     //let draft=element.all(by.tagName('a')).last();
+    //draft.click();
+    element(by.cssContainingText('a', 'Jira Confluence')).click();
+    element(by.id('Publicar')).click();
+  }
+
+  closeAdminSesion(){
+    element(by.cssContainingText('a','Santiago')).click();
+    element(by.cssContainingText('a','Cerrar sesión')).click();
+    return element(by.cssContainingText('a','¿Eres miembro conectate?')).getText();
+  }
+
+  closeGitSesion(){
+    element(by.cssContainingText('a','Carlos')).click();
+    element(by.cssContainingText('a','Cerrar sesión')).click();
+    return element(by.cssContainingText('a','¿Eres miembro conectate?')).getText();
+  }
+
+  getNewTool(){
+    return element(by.cssContainingText('a','Jira Confluence')).getText();
+  }
+
+  navigateToAddExamplePage() {
+    element(by.cssContainingText('a', 'Jira Confluence')).getText();
+    element(by.id('Add')).click();
+    return element(by.css('.col-md-12 h1')).getText();
+  }  
+
+  editExampleDraftInputs() {
+    element(by.id('name')).sendKeys('Ejemplo1');
+    element(by.name('description')).sendKeys('Desc');
+    element(by.name('Instrucciones')).sendKeys('Leer capitulo 2');
+    browser.sleep(2000);
+    element(by.id('nombre')).sendKeys('Manifiesto Agil');
+    element(by.id('Enlace')).sendKeys('http://www.agilemanifesto.org');
+    browser.sleep(2000);
+    
+  }
+
+  editExampleDraftResource(){
+    element(by.xpath('//span[contains(text(), "Agregar recurso")]')).click();
+    let secondName= element.all(by.id('nombre')).last();
+    secondName.sendKeys('Libro');
+    browser.sleep(2000);
+    let secondUrl= element.all(by.id('Enlace')).last();
+    secondUrl.sendKeys('otro');
+    element(by.xpath('//span[contains(text(), "Agregar recurso")]')).click();
+    let lasDelete=element.all(by.css('.fas.fa-trash')).last();
+    lasDelete.click();
+  }
+
+  selectExampleDraftOptions() {
+    element(by.name('discipline')).click();
+    browser.sleep(2000);
+    element(by.xpath('//span[contains(text(), "Ciencias exactas")]')).click();
+    element(by.name('pedagogicStrategy')).click();
+    element(by.xpath('//span[contains(text(), "Blended")]')).click();
+    browser.sleep(2000);
+   }
+
+   saveExampleDraft(){
+    element(by.id('Guardar')).click();
+    return element(by.cssContainingText('.mat-card-title', 'Ejemplo1')).getText(); 
+   }
+
+   navigateToAddTutorialPage() {
+    browser.sleep(3000);
+    element(by.cssContainingText('a', 'Moodle')).click();
+    browser.sleep(3000);
+    element(by.id('btnAgregarTutorial')).click();
+    return element(by.css('.col-12 h2')).getText();
+   }
+
+   editTutorialDraftInputs() {
+    element(by.id('txtNombre')).sendKeys('Tutorial Angular');
+    element(by.name('txtUrl')).sendKeys('https://www.youtube.com/watch?v=Sx_2dOYOtes');
+       
+   }
+
+   saveTutorialDraft(){
+    element(by.id('SaveTut')).click();
+    element(by.id('CancelTut')).click();
+    return element(by.cssContainingText('.mat-card-title', 'Tutorial Angular')).getText(); 
+   }
+
+/*
+
+
+
+
+
+
+  
+
+
 
   returnHome(){
     browser.get('/');
@@ -105,10 +242,7 @@ export class AppPage {
     return tool.getText();
   }
 
-  cancelToolDraft(){
-
-    element(by.id('Cancelar')).click();    
-  }
+  
 
   editExampleDraftInputs() {
     element(by.id('Add')).click();
@@ -153,15 +287,11 @@ export class AppPage {
     return element(by.tagName('h2')).getText();
   }
 
-  approveDraft(){
-    let draft=element.all(by.tagName('a')).last();
-    draft.click();
-    element(by.id('Approve')).click();
-  }
+
 
   publishDraft(){
     browser.sleep(3000);
     element(by.buttonText('Publicar')).click();
-  }
+  }*/
 
 }
